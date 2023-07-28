c2c0e165c529bc496b0c61e95acb0b506b88f214
ValentineOgede <ogedevalentine@gmail.com>
2023-07-28 10:14:57 +0100

    first commit

---
+++ main/static/js/load.js
@@ -0,0 +1,10 @@
+coast loader = document.querySelector("loader.con");
+
+window.addEventListener("load", function () {
+    this.setTimeout(myload, 1000)
+}
+);
+
+function myload() {
+    loader.style.display = "none";
+}
